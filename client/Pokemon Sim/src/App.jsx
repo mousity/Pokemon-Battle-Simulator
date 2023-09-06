d19f28258dccdfdc11249e3a4136c47721236877
Samr Mouna <samr.mouna@gmail.com>
2023-09-05 22:52:09 -0400

    Practice using socket.io for basic messaging

--- client/Pokemon Sim/src/App.jsx
+++ client/Pokemon Sim/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:4000")
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "hello"});
+  };
 
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    })
+  }, [socket])
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input placeholder='Message...' />
+        <button onClick={sendMessage}>Send Message</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const port = 4000;
 const { Server } = require('socket.io');
 const cors = require('cors');
 
 const server = http.createServer(app);
+app.use(express.json());
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
-app.use((req,res,next)=>{
+app.use((req, res, next)=>{
     res.on("finish",()=>{
         console.log(`${req.method} ${req.originalUrl} ${req.statusCode} `);
     });
     next();
 });
 
-app.use(express.json());
-
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST", "PATCH", "DELETE"]
     }
 })
 
+io.on("connection", (socket) => {
+    console.log(`user connected: ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("receive_message", data);
+    })
+})
