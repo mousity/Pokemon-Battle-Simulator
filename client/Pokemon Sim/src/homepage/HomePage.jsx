21234795666b77bf070cbd1d39a0d019442f484c
Samr Mouna <samr.mouna@gmail.com>
2023-09-14 18:48:44 -0400

    initializing landing components

--- client/Pokemon Sim/src/homepage/HomePage.jsx
+++ client/Pokemon Sim/src/homepage/HomePage.jsx
@@ -1,7 +1,8 @@
 import react from "react";
 
 return (
-    <>
-    
-    </>
+    <div>
+        <h2>Welcome to my battle sim!</h2>
+        <p>Every battle is random, consisting of generations 1 and 2 thus far. Enter a room to connect with a friend!</p>
+    </div>
 )
---
+++ client/Pokemon Sim/src/navbar/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+return (
+    <nav>
+        <h1>Logo Placeholder for now!</h1>
+    </nav>
+)
