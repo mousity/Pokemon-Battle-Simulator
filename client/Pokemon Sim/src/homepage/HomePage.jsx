d103d9336d2ea14cd0bdcc9b8c88a15b94ac6823
Samr Mouna <samr.mouna@gmail.com>
2023-09-14 19:31:55 -0400

    naming case fix

--- client/Pokemon Sim/src/homepage/HomePage.jsx
+++ client/Pokemon Sim/src/homepage/HomePage.jsx
@@ -1,8 +1,13 @@
 import react from "react";
 
+function HomePage(){
 return (
     <div>
         <h2>Welcome to my battle sim!</h2>
         <p>Every battle is random, consisting of generations 1 and 2 thus far. Enter a room to connect with a friend!</p>
+        <form>
+            <input type="number" name="roomNumber" className="roomNumber" placeholder="Room Number..."/>
+        </form>
     </div>
 )
+}
--- client/Pokemon Sim/src/main.jsx
+++ client/Pokemon Sim/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import HomePage from './homepage/HomePage.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
