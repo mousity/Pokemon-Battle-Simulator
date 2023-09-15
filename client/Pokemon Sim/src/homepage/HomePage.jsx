2257c5092e7cb4347782e05bc3acc95e853afd49
Samr Mouna <samr.mouna@gmail.com>
2023-09-14 20:00:09 -0400

    simple button add

--- client/Pokemon Sim/src/homepage/HomePage.jsx
+++ client/Pokemon Sim/src/homepage/HomePage.jsx
@@ -7,6 +7,7 @@ return (
         <p>Every battle is random, consisting of generations 1 and 2 thus far. Enter a room to connect with a friend!</p>
         <form>
             <input type="number" name="roomNumber" className="roomNumber" placeholder="Room Number..."/>
+            <input type="submit"/>
         </form>
     </div>
 )
