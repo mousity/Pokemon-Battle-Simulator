22988c58252d793f40d895700cb2f4465aeda7c5
Samr Mouna <samr.mouna@gmail.com>
2023-09-20 17:20:33 -0400

    More informative message on homepage

--- client/Pokemon Sim/src/homepage/HomePage.jsx
+++ client/Pokemon Sim/src/homepage/HomePage.jsx
@@ -4,7 +4,9 @@ function HomePage(){
 return (
     <div>
         <h2>Welcome to my battle sim!</h2>
-        <p>Every battle is random, consisting of generations 1 and 2 thus far. Enter a room to connect with a friend!</p>
+        <p>Every pokemon battle is random, consisting of generations 1 and 2 thus far.
+             Though not perfect, an basic algorithm is in place to generate decent movesets for your pokemon. Enter a room to connect with a friend!
+              Not functional as of yet.</p>
         <form>
             <input type="number" name="roomNumber" className="roomNumber" placeholder="Room Number..."/>
             <input type="submit"/>
